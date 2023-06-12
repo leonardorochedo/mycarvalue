fcedcba69fc004bd9378389755d781a2d48bafd0
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2023-06-12 18:22:04 -0300

    add: service to create user, route of this method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
+// TypeORM | Repository
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectRepository(User) private repo: Repository<User>) {} // Repository is defined automaticly from TypeORM
+
+    create(email: string, password: string) {
+        const user = this.repo.create({ email, password }) // Create a instance of User
+
+        return this.repo.save(user); // Save a isntance of this User in Database
+    }
+}
