1c41f5d4ab04f4fa4bcd82891016105c97777e71
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2023-06-21 11:06:52 -0300

    add: findUsers, findUser and signin user tests in users controller

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -3,6 +3,7 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
 import { User } from './user.entity';
+import { NotFoundException } from '@nestjs/common';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -11,6 +12,13 @@ describe('UsersController', () => {
 
   beforeEach(async () => {
     fakeUsersService = {
+      findAll: () => {
+        const users = [];
+
+        users.push({ id: 1, email: 'asdf@asdf.com', password: 'asdf' } as User);
+
+        return Promise.resolve(users);
+      },
       findOne: (id: number) => {
         return Promise.resolve({ id, email: 'asdf@asdf.com', password: 'asdf' } as User);
       },
@@ -26,7 +34,9 @@ describe('UsersController', () => {
     };
     fakeAuthService = {
       // signup: () => {},
-      // signin: () => {},
+      signin: (email: string, password: string) => {
+        return Promise.resolve({ id: 1, email, password } as User);
+      },
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -49,4 +59,28 @@ describe('UsersController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  // user tests
+  it('findAllUsers returns a list of users with an email', async () => {
+    const users = await controller.findAllUsers();
+
+    expect(users).toBeDefined();
+  });
+
+  it('findUser returns a single user with the given id', async () => {
+    const user = await controller.findUser('1');
+
+    expect(user).toBeDefined();
+  });
+
+  it('signin updates session object and returns user', async () => {
+    const session = { userId: -10 };
+    const user = await controller.signin(
+      { email: 'asdf@asdf.com', password: 'asdf' },
+      session
+    );
+
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1);
+  })
 });
