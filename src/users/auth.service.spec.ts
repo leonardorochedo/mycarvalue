ff54b2eeae055f759a994900e258f1b6ce401181
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2023-06-21 09:48:00 -0300

    add: signin method in auth.service test file

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -2,7 +2,7 @@ import { Test } from "@nestjs/testing"
 import { AuthService } from "./auth.service"
 import { UsersService } from "./users.service";
 import { User } from "./user.entity";
-import { BadRequestException } from "@nestjs/common";
+import { BadRequestException, NotFoundException } from "@nestjs/common";
 
 // title test file
 describe('AuthService', () => {
@@ -51,6 +51,12 @@ describe('AuthService', () => {
     it('throws an error if user signs up with email that is in use', async () => {
         fakeUsersService.findEmail = () => Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
 
-        await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(BadRequestException);
+        await expect(service.signup('asdf@asdf.com', 'asdf'))
+        .rejects.toThrow(BadRequestException);
     });
+
+    it('throws if signin is called with an unused email', async () => {
+        await expect(service.signin('asdflkj@asdlfkj.com', 'passdflkj'),)
+        .rejects.toThrow(NotFoundException);
+    })
 })
