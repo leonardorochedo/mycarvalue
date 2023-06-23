01627df27ca0f2f54b7332b845be729513180a11
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2023-06-23 12:31:18 -0300

    add: return in reports create controller, to be serialized created a reportdto and apply with serialize interceptor in report controller

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -5,13 +5,18 @@ import { ReportsService } from './reports.service';
 
 // Dto's
 import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportDto } from './dtos/report.dto';
 
 // Guard's
 import { AuthGuard } from '../guards/auth.guard';
 import { CurrentUser } from '../users/decorators/current-user.decorator';
 import { User } from '../users/user.entity';
 
+// Serialize
+import { Serialize } from '../interceptors/serialize.interceptor';
+
 @Controller('reports')
+@Serialize(ReportDto)
 export class ReportsController {
     constructor(private reportsService: ReportsService) {}
 
