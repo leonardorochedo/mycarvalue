2c2365616101bec7e4c86307596e4f1075165919
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2023-06-27 12:00:06 -0300

    add: change setup file test to funcionally with migration db

--- test/setup.ts
+++ test/setup.ts
@@ -1,9 +1,15 @@
 import { rm } from "fs/promises";
 import { join } from "path";
+import { getConnection } from "typeorm";
 
 // remove test db befora tests
 global.beforeEach(async () => {
     try {
         await rm(join(__dirname, '..', 'test.sqlite'))
     } catch (err) {}
+});
+
+global.afterEach(async () => {
+    const conn = getConnection();
+    await conn.close();
 });
